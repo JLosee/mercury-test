a5f3fd8dde6792df23b88b334bd70a2bbc51b429
Jeffrey Losee <jeff.losee@gmail.com>
2022-12-13 15:47:42 -0700

    setup gh-pages

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,7 @@ export const LeftQuote = ({ children }) => {
     return (
         <Grow in>
             <Box>
-                <Box sx={{ width: 140, height: 140, opacity: .1, mt: 2, marginLeft: '0px', marginBottom: '-100px', backgroundRepeat: 'no-repeat', backgroundSize: '140px, 140px', backgroundImage: "url('https://jlosee.github.io/mercury-test/left-quote.png')" }} />
+                <Box sx={{ width: 140, height: 140, opacity: .1, mt: 2, marginLeft: '0px', marginBottom: '-100px', backgroundRepeat: 'no-repeat', backgroundSize: '140px, 140px', backgroundImage: "url('./left-quote.png')" }} />
                 <Typography sx={{ mt: 2, ml: 3 }} variant="h4">{children}</Typography>
             </Box>
         </Grow>
@@ -14,7 +14,7 @@ export const RightQuote = ({ children }) => {
     return (
         <Grow in>
             <Box sx={{ mr: 5, position: 'relative' }}>
-                <Box sx={{ width: 140, height: 140, opacity: .1, mt: 2, position: 'absolute', right: -50, top: -30, marginBottom: '-80px', backgroundRepeat: 'no-repeat', backgroundSize: '140px, 140px', backgroundImage: "url('https://jlosee.github.io/mercury-test/left-quote.png')" }} />
+                <Box sx={{ width: 140, height: 140, opacity: .1, mt: 2, position: 'absolute', right: -50, top: -30, marginBottom: '-80px', backgroundRepeat: 'no-repeat', backgroundSize: '140px, 140px', backgroundImage: "url('./right-quote.png')" }} />
                 <Typography sx={{ mt: 2, textAlign: 'right' }} variant="h4">{children}</Typography>
             </Box>
         </Grow>
